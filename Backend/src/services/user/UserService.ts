0f1f63aa52de7f05d8db574515f4f2488fac004f
Oluwafemi Akinde <oluwafemiakinde@gmail.com>
2020-09-24 22:02:03 +0400

    more updates

--- Backend/src/services/user/UserService.ts
+++ Backend/src/services/user/UserService.ts
@@ -1,3 +1,4 @@
+import { IUser } from "./user.schema";
 
 
 export class UserService {
@@ -6,5 +7,9 @@ export class UserService {
 
     login() {
         
+    }
+
+    getUserInfo(): Promise<IUser> {
+
     }
 }
